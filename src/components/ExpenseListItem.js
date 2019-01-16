52015c4cc1fd275d468f7b5b4ca166bbc4046fed
Jim Whurr <jim.whurr@outlook.com>
2019-01-16 11:18:35 +0000

    138. New Feature Workflow

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment'
+import numeral from 'numeral';
+import 'numeral/locales/en-gb';
+
+numeral.locale('en-gb');
 
 const ExpenseListItem = ( { id, description, amount, createdAt }) => (
     <div>
         <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-        <p>{amount} {createdAt}</p>
+        <p>
+        {numeral(amount/100).format('$0,0.00')}
+        -
+        {moment(createdAt).format('Do MMMM YYYY')}
+        </p>
     </div>
 );
 
