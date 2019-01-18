6259307fae7dd757074ad9b953ce7a618ec7c691
Jim Whurr <jim.whurr@outlook.com>
2019-01-18 11:39:13 +0000

    Storing array data in firebase - II

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import * as firebase from 'firebase';
-import expenses from '../tests/fixtures/expenses';
+// import expenses from '../tests/fixtures/expenses';
 
 const config = {
     apiKey: "AIzaSyAT4cHGT6trlwWjCa3lePSXC8dbrrsB454",
@@ -14,22 +14,55 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-const ids = [];
+// database.ref('expenses')
+//     .once('value')
+//     .then( (snapshot) => {
+//         console.log(snapshot.val());
+//     });
+// ^^^ returns our objects but not as an array, we need to process the data somehow
+// to get the array that we need for redux. Look up 'data snapshot' for methods
+// other than val()... use forEach() to iterate over expenses and get 'child' snapshots.
 
-for(let i = 0; i < expenses.length; i++) {
-    const data = {
-        description: expenses[i].description,
-        note: expenses[i].note,
-        amount: expenses[i].amount,
-        createdAt: expenses[i].createdAt,
-    }
+// const displayValues = (snapshot) => {
+//     const expenses = [];
 
-    ids[i] = database.ref('expenses').push(data);
-}
+//     snapshot.forEach( (childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         })
+//     })
 
-for (let j = 0; j < ids.length; j++) {
-    console.log(`expenses[${j}].id: ${ids[j].key}`);
-}
+//     console.log(expenses);
+//  };
+
+// database.ref('expenses')
+//     .on('value', displayValues);
+
+database.ref('expenses').on('child_changed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
+});
+
+database.ref('expenses').on('child_added', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
+});
+
+// const ids = [];
+
+// for(let i = 0; i < expenses.length; i++) {
+//     const data = {
+//         description: expenses[i].description,
+//         note: expenses[i].note,
+//         amount: expenses[i].amount,
+//         createdAt: expenses[i].createdAt,
+//     }
+
+//     ids[i] = database.ref('expenses').push(data);
+// }
+
+// for (let j = 0; j < ids.length; j++) {
+//     console.log(`expenses[${j}].id: ${ids[j].key}`);
+// }
 
 // firebase does not store arrays, it stores data under an objectt that is a
 // number - the index of thhe element in the array! TThhis is not that useful.
