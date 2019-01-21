ec7b279ebe60ae50cc66c47fc460b74baf191778
Jim Whurr <jim.whurr@outlook.com>
2019-01-21 19:27:49 +0000

    164. Basic Auth working but all routes still public

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
 import { startSetExpenses } from './actions/expenses';
@@ -22,17 +22,29 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+let hasRendered = false;
+const renderApp = () => {
+    if ( ! hasRendered ) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+};
 
-store.dispatch( startSetExpenses() ).then( () => {
-    ReactDOM.render(jsx, document.getElementById('app'));
-});
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged( (user) => {
     if (user) {
         console.log('login');
+        store.dispatch( startSetExpenses() ).then( () => {
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
+        
     }
     else {
-        console.log('logout');
+        renderApp();
+        history.push('/');
     }
 });
