d2ad55a2c6de93e7ad881b7887ecaa482fcb96c0
Jim Whurr <jim.whurr@outlook.com>
2019-01-23 09:00:05 +0000

    180. Added @babel/polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = (env) => {
     // const CSSExtract = new ExtractTextPlugin('styles.css');
 
     return {
-        entry: /*'./src/playground/hoc.js',*/  './src/app.js',
+        entry: ['@babel/polyfill', './src/app.js'],
         output: {
             path: path.join(__dirname,'public','dist'),
             filename: 'bundle.js'
